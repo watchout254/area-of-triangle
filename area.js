21638c2a7d7a895db2c8a53de64c169324f249c0
Daniel Mukenya <88248852+watchout254@users.noreply.github.com>
2022-05-10 04:46:11 -0800

    area of triangle

---
+++ area.js
@@ -0,0 +1,6 @@
+const base = window.prompt("Enter the base of the triangle:");
+const height = window.prompt("Enter the height of triangle:");
+
+const area = 1/2 * base * height;
+
+document.getElementById("Area").innerHTML = 1/2 * base *height;
